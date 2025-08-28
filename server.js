2f772e4c6658eeaf2323e836d814b9638ba9d893
yanish9 <yanishguru@gmail.com>
2025-08-28 13:41:32 -0400

    set lights

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.post("/api/color", (req, res) => {
   const { id, color } = req.body || {};
   // Log exactly what arrived
   console.log(`[COLOR] Button ${id} -> ${color}`);
+
+  setColor(req);
   // You could forward to hardware here, write to a DB, etc.
   res.json({ ok: true });
 });
@@ -19,3 +21,42 @@ app.post("/api/color", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
+function setColor(req){
+  
+  const item = req.body;
+
+  console.log("__________________")
+  console.log(item) 
+  console.log("__________________")
+ 
+
+  // item.nid = nid;
+
+  var jsonString = JSON.stringify(item);
+
+  jsonString = "'" + jsonString + "'";
+  console.log(jsonString);
+
+  const scriptPath = path.join('/home/yan/sx126x_lorawan_hat_code/python/lora/examples/SX126x/', 'transmitter_set_color.py');
+
+  // var l = item.color;
+  // l = l.substring(l.indexOf("(")+1, l.lastIndexOf(")"))
+  // Run the Python script with RGB values as arguments
+  exec(`sudo python3 ${scriptPath} ${jsonString}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error: ${stderr}`);
+      // return res.status(500).send('Error executing Python script');
+    }
+
+    // Send success response with script output
+    console.log(`Script Output: ${stdout}`);
+    // res.status(200).json({
+    //   status: 'success',
+    //   message: `Color RGB(${item.color}) saved successfully`,
+    // });
+  });
+
+  res.status(200).send('Data saved successfully');
+}
